151e669c1cc73b9124fed4044cdc66af42d77b6d
Rafael Yalico Campos <ryalico83@gmail.com>
2025-04-28 09:36:55 -0500

    añadir .gitignore y actualizar scripts

--- server.py
+++ server.py
@@ -143,4 +143,12 @@ async def root():
 # Arranque de Uvicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run(
+        "server:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False,
+        log_level="info",
+        access_log=True
+    )
+
